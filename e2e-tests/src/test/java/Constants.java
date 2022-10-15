2ce8a17c7cb177c96f9a3d151c829b3cb8b54a26
d <dthill@gmx.net>
2022-10-15 22:26:17 +0200

    fix e2e

--- e2e-tests/src/test/java/Constants.java
+++ e2e-tests/src/test/java/Constants.java
@@ -1,5 +1,5 @@
 public class Constants {
 
-    public static final String TESTSERVER = "http://selenium:4444";
+    public static final String TESTSERVER = "http://localhost:4444";
     public static final String WEBSITE = "http://medicare-2129249250.eu-central-1.elb.amazonaws.com";
 }
